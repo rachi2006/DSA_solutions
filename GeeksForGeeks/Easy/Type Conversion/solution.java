9d431380a5092f40437aa6aad15ec83da8102758
Rachith Kumar M <rachirachith8@gmail.com>
2026-07-21 22:43:42 +0530

    [Syncode] GeeksForGeeks/Easy/Type Conversion

---
+++ GeeksForGeeks/Easy/Type Conversion/solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+
+        // code here
+        int a = (int)d;
+        System.out.println(a);
+    }
+}
