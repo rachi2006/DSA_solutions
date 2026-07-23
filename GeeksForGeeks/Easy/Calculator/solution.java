8b583e226c832aac9f4129e2b948d9c6259d9452
Rachith Kumar M <rachirachith8@gmail.com>
2026-07-23 21:23:23 +0530

    [Syncode] GeeksForGeeks/Easy/Calculator

---
+++ GeeksForGeeks/Easy/Calculator/solution.java
@@ -0,0 +1,13 @@
+class Solution { 
+    public void calculate(int a, int b, int operator) { 
+        if (operator == 1) { 
+            System.out.print(a + b); 
+        } else if (operator == 2) { 
+            System.out.print(a - b); 
+        } else if (operator == 3) { 
+            System.out.print(a * b); 
+        } else { 
+            System.out.print("Invalid Input"); 
+        } 
+    } 
+}
